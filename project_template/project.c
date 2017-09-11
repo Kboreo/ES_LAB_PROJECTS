1ce65eec09cd45e39b53ee366f663b3488599df3
Kboreo <kboreo@ltu.edu>
2017-09-11 17:33:45 -0400

    Added Comments and more!
    Added some comments that were missing and also added another loop, so if buttons SW1 and SW2 are both pressed then a blue will blink.

--- project_template/project.c
+++ project_template/project.c
@@ -50,20 +50,21 @@ int  main(void)
     
 		}
 		
-		UnlockPins();
+		UnlockPins();		//Function that unlocks certain pins that are needed.
     
 		// Enable the GPIO pin for the LED (PF3).  Set the direction as output, and
     // enable the GPIO pin for digital function.
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3); // set pin 3 as output
-		GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0); // set pin 4 as input
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2); // set pin 3and 2 as output
+		GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0); // set pin 4 and 0 as input
 		//these two switchs need an internal pull up on pins
 		GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_0|GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);//set internal pullup R for pin 0 and 4
 		
-		int x;
+		int x;  //Int that contains the output of the button SW2
+		int y;  //Int that contains the output of the button SW1
 		
-		x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+		x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0); // set x equal to the output of button SW2 
 		
-		while(x == 1)
+		while(x == 1)  //While loop that keeps checking until x does not equal 1, meaning SW2 was pushed. 
 		{
 			x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);	
 		}
@@ -71,6 +72,29 @@ int  main(void)
 		
     while(1)
     {
+			x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0); // set x equal to the output of button SW2
+			y = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4); // set x equal to the output of button SW1
+			
+			if (x && y == 0) //if loop that checks if SW1 and SW2 are pressed, if they are both pressed, blue LED will light.
+			{
+				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0xF);  // Turn on blue LED
+				
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        {
+        }
+
+        // Turn off the LED.
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0x0);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
+        {
+        }
+				
+			}
+			else  //Else green LED will light.
+			{
 				UARTCharPut(UART0_BASE, temp);
 			  temp++;
 				//UARTCharPut(UART0_BASE, '\n');
@@ -90,6 +114,7 @@ int  main(void)
         for(ui32Loop = 0; ui32Loop < 200000; ui32Loop++)
         {
         }
+			}
     }
 }
 
