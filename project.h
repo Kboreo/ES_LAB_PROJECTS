d98ccbcb46b874073b2bde167d80b2350b862b06
ptesolin <ptesolin@ltu.edu>
2017-09-13 10:50:49 -0400

    Working Code 1 input

--- project.h
+++ project.h
@@ -42,3 +42,15 @@
 #include "tm4c123gh6pm.h"
 
 #include "src/Uart_helper.h"
+
+
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "sysctl.h"
+#include "hw_memmap.h"
+#include "gpio.h"
+#include "uart.h"
+#include "../inc/tm4c123gh6pm.h"
+#include "hw_gpio.h"
+#include "hw_types.h"
