435cfc3ce6e9ccb2c3bc3c92854ed62b23bedf72
ptesolin <ptesolin@ltu.edu>
2017-09-14 12:13:28 -0400

    ADC

--- project.c
+++ project.c
@@ -22,6 +22,14 @@ void SetupHardware()
 //	UartSetup2();
 }
 
+//ui32Base is the base address of the ADC to configure, which must always be ADC0_BASE.
+//ui32Config is a combination of the ADC_CLOCK_SRC_ and ADC_CLOCK_RATE_ values
+//used to configure the ADC clock input.
+//ui32ClockDiv is the input clock divider for the clock selected by the ADC_CLOCK_SRC value.
+void ADCClockConfigSet(uint32_t ui32Base, uint32_t ui32Config, uint32_t ui32ClockDiv);
+
+
+
 void UnlockPins()
 {
 	//Need to Unlock PF4
@@ -34,20 +42,21 @@ void UnlockPins()
 int  main(void)
 {
 	
-
+		//*******************************
+		//Setup/unlock pins as inputs and outputs
 		
 		uint8_t temp;
     volatile uint32_t ui32Loop;
 	
 	
 	
-    // Enable the GPIO port that is used for the on-board LED.
-		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    // Enable the GPIO port that is used for the on-board LED and pwm.
+		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF | SYSCTL_PERIPH_GPIOA | SYSCTL_PERIPH_GPIOE);
     SetupHardware();
 
 		
 		// Check if the peripheral access is enabled.
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF | SYSCTL_PERIPH_GPIOA | SYSCTL_PERIPH_GPIOE))
     {
     
 		}
@@ -56,12 +65,16 @@ int  main(void)
     
 		// Enable the GPIO pin for the LED (PF 3, 2, 1).  Set the direction as output, and
     // enable the GPIO pin for digital function.
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1); // set pin 1,2,3 as output
-		GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0); // set pin 0 and 4 as input
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1); // set Port F pin 1,2,3 as output
+		GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4); // set Port A pin 2,3,4 as output
+		GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4|GPIO_PIN_0); // set PORT F pin 0 and 4 as input
+		GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2); // set PORT E pin 1 and 2 as input
 		//these two switchs need an internal pull up on pins
 		GPIOPadConfigSet(GPIO_PORTF_BASE,GPIO_PIN_0|GPIO_PIN_4,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);//set internal pullup R for pin 0 and 4
+		GPIOPadConfigSet(GPIO_PORTE_BASE,GPIO_PIN_1|GPIO_PIN_2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_ANALOG); //set port E pin 1,2 as analog input
 		
 		
+		// set x equal to the value of switch 1
 		int x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
 		 
 		
@@ -69,18 +82,67 @@ int  main(void)
 		
     while(1)
     {
+				//Reads the value of switch 2 while in loop
+				//Will flash on board blue LED when switch is pressed 
+				x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+			
+			int delay_on;
+			int delay_off; 
+			
+			while(x==1)
+		{
+			
+				//Reads the value of switch 2 while in loop
+				//Will flash on board blue LED when switch is pressed 
+				x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+				
+			
+
+
+
+			
+					
+		
+				
+      // Turn on the LED.
+       GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, 0xFF);
+
+
+       // Delay for a bit.
+       for(ui32Loop = 0; ui32Loop < delay_on; ui32Loop++)
+        {
+        }
+
+        // Turn off the LED.
+        GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, 0x0);
+
+        // Delay for a bit.
+        for(ui32Loop = 0; ui32Loop < delay_off; ui32Loop++)
+        {
+        }
+				
+    }
+			
+			
+			
+			
+			
+			
+			//**************************
+			// Flash LED Blue when switch 1 is pressed
+			//Reads the value of switch 2 
 			x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
 			
+			//Loop will run, blue LED flashes if switch 2 is pressed | == 0
 			while(x == 0)
 		{
-			  x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);	
+				//Reads the value of switch 2 while in loop
+				//Will exit loop when switch is released 
+				x = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);	
 		
-				UARTCharPut(UART0_BASE, temp);
-			  temp++;
-				//UARTCharPut(UART0_BASE, '\n');
-				//UARTCharPut(UART0_BASE, '\r');
+				
         // Turn on the LED.
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0xF);
+        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0xFF);
 
 
         // Delay for a bit.
