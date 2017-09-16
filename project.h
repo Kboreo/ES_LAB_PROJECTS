5818508d69d30f2f462747d125f9eded1ae5040a
ptesolin <ptesolin@ltu.edu>
2017-09-16 10:10:14 -0400

    Working A to D

--- project.h
+++ project.h
@@ -54,3 +54,17 @@
 #include "../inc/tm4c123gh6pm.h"
 #include "hw_gpio.h"
 #include "hw_types.h"
+
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "sysctl.h"
+#include "hw_memmap.h"
+#include "gpio.h"
+#include "adc.h"
+#include "uart.h"
+#include "tm4c123gh6pm.h"
+#include "hw_gpio.h"
+#include "hw_types.h"
+
+void SetupADC(void); 
