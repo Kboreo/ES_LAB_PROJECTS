3c12dc02c2fa1d051f9426ce0ad6e903844dd089
ptesolin <ptesolin@ltu.edu>
2018-03-22 17:35:03 -0400

    try

--- project.c
+++ project.c
@@ -1,110 +1,139 @@
-/*********************************************************************************************************************************
-**
-** Module Name:   project.c
-**
-** Module Description: Main Project C file 
-**
-**********************************************************************************************************************************
-**
-** Author(s):		Kodey Boreo, Phillip Tesolin 
-**
-**********************************************************************************************************************************/
-
-//Includes
-#include "project.h"
-#include <stdio.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include <stdint.h>
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
 
-#ifdef DEBUG
-
-void
-__error__(char *pcFilename, uint32_t ui32Line)
+//initialize I2C module 0
+//Slightly modified version of TI's example code
+void InitI2C0(void)
 {
-}
-#endif
-
-//Function Protoypes
-void mainMenu(char ctemp,uint8_t temp,uint32_t ui32Loop);  //Main menu that displays to the UART terminal for a HMI
-void SetupHardware(void);  //Function that sets up all of the pins,ports, etc... that are used in the program
-
-//Main Code
-int  main(void)
-{		
-	//Variables
-	char ctemp; //Temp char used for UART
-	uint8_t temp;	//Temp 8-bit int used for gpio functions
-	volatile uint32_t ui32Loop; //32-bit int used for multiple functions 
-	
-	SetupHardware();
-	mainMenu(ctemp, temp, ui32Loop);
-}	
-	
-//Main menu that displays to the UART terminal for a HMI	
-void mainMenu(char ctemp,uint8_t temp,uint32_t ui32Loop)
-{	
-	//While loop for main user menu	
-while (1)
-{
-		printf("What function would you like to test?\n 1.GPIO\n 2.PWM\n 3.UART\n 4.Systick\n 5.NVIC\n 6.ADC\n"); //Askes user what function they would like to use.
-		ctemp = getc(stdin); //Gets a char from the terminal from the user.
-		printf("You entered %c\n\n",ctemp); //Lets the user know what they have selected
-		
-		switch (ctemp)		//Switch statement that selects which function depending on the user input.
-		{
-			//GPIO
-			case '1':						
-				pinReadAndWrite(ui32Loop, temp); 
-				break;	
-			
-			//PWM
-			case '2':			
-				setupPWM(); //Sets up PWM
-				pwmTest();
-				break;
-			
-			//Uart				
-			case '3':
-				setup_UART(ctemp);
-				break;
-			
-			//systick
-			case '4':				
-				printf("Blue LED will stay on for 5 seconds!\n\n");		
-				SysTickWait10msDN(100);
-				break;
-			
-			//NVIC
-			case'5':				
-				printf ("No idea\n\n");
-				break;
-			
-			//ADC
-			case'6':				
-				ADCReadChan();
-				break;
-			
-			//Default case if user types anything other than the given options
-			default:				
-				printf("Ya done messed up, try again!\n\n");
-				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x0);  // Turn off LED
-				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0x0);  // Turn off LED
-				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0x0);  // Turn off LED
-				break;		
-		}
-	}	
-}
-
-//Function that sets up all of the pins,ports, etc... that are used in the program
-void SetupHardware(void)  
-{
-	UnlockPins();	//Unlocks pins that are used so they can be inputs or outputs
-	UartSetup();	//Sets up Uart communication using RealTerm
-	setup_IO();		//UnlockPins();		//Function that unlocks certain pins that are needed.
-	SetupSystickDN();	//Sets up the timers and hardware to use systick
-	SetupADC();	//Hardware setup for ADC	
+    //enable I2C module 0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+ 
+    //reset module
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+     
+    //enable GPIO peripheral that contains I2C 0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+ 
+    // Configure the pin muxing for I2C0 functions on port B2 and B3.
+    GPIOPinConfigure(GPIO_PB2_I2C0SCL);
+    GPIOPinConfigure(GPIO_PB3_I2C0SDA);
+     
+    // Select the I2C function for these pins.
+    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+    GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+ 
+    // Enable and initialize the I2C0 master module.  Use the system clock for
+    // the I2C0 module.  The last parameter sets the I2C data transfer rate.
+    // If false the data rate is set to 100kbps and if true the data rate will
+    // be set to 400kbps.
+    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), false);
+     
+    //clear I2C FIFOs
+    HWREG(I2C0_BASE + I2C_O_FIFOCTL) = 80008000;
 }
 
 
+//sends an I2C command to the specified slave
+void I2CSend(uint8_t slave_addr, uint8_t num_of_args, ...)
+{
+    // Tell the master module what address it will place on the bus when
+    // communicating with the slave.
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, false);
+     
+    //stores list of variable number of arguments
+    va_list vargs;
+     
+    //specifies the va_list to "open" and the last fixed argument
+    //so vargs knows where to start looking
+    va_start(vargs, num_of_args);
+     
+    //put data to be sent into FIFO
+    I2CMasterDataPut(I2C0_BASE, va_arg(vargs, uint32_t));
+     
+    //if there is only one argument, we only need to use the
+    //single send I2C function
+    if(num_of_args == 1)
+    {
+        //Initiate send of data from the MCU
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+         
+        // Wait until MCU is done transferring.
+        while(I2CMasterBusy(I2C0_BASE));
+         
+        //"close" variable argument list
+        va_end(vargs);
+    }
+     
+    //otherwise, we start transmission of multiple bytes on the
+    //I2C bus
+    else
+    {
+        //Initiate send of data from the MCU
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+         
+        // Wait until MCU is done transferring.
+        while(I2CMasterBusy(I2C0_BASE));
+         
+        //send num_of_args-2 pieces of data, using the
+        //BURST_SEND_CONT command of the I2C module
+        for(uint8_t i = 1; i < (num_of_args - 1); i++)
+        {
+            //put next piece of data into I2C FIFO
+            I2CMasterDataPut(I2C0_BASE, va_arg(vargs, uint32_t));
+            //send next data that was just placed into FIFO
+            I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_CONT);
+     
+            // Wait until MCU is done transferring.
+            while(I2CMasterBusy(I2C0_BASE));
+        }
+     
+        //put last piece of data into I2C FIFO
+        I2CMasterDataPut(I2C0_BASE, va_arg(vargs, uint32_t));
+        //send next data that was just placed into FIFO
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+        // Wait until MCU is done transferring.
+        while(I2CMasterBusy(I2C0_BASE));
+         
+        //"close" variable args list
+        va_end(vargs);
+    }
+}
 
-
-
+//read specified register on slave device
+uint32_t I2CReceive(uint32_t slave_addr, uint8_t reg)
+{
+    //specify that we are writing (a register address) to the
+    //slave device
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, false);
+ 
+    //specify register to be read
+    I2CMasterDataPut(I2C0_BASE, reg);
+ 
+    //send control byte and register address byte to slave device
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+     
+    //wait for MCU to finish transaction
+    while(I2CMasterBusy(I2C0_BASE));
+     
+    //specify that we are going to read from slave device
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, true);
+     
+    //send control byte and read from the register we
+    //specified
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+     
+    //wait for MCU to finish transaction
+    while(I2CMasterBusy(I2C0_BASE));
+     
+    //return data pulled from the specified register
+    return I2CMasterDataGet(I2C0_BASE);
+}
